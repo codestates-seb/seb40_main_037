0f9cb53ff27e4202e5e6e2f3603b3df4989cda0f
pung8146 <pung8146@naver.com>
2022-11-16 21:37:33 +0900

    :sparkles: feat : 리뷰이미지 map함수로 뿌리기 기능 추가 #3

--- client/src/components/Items/Item.jsx
+++ client/src/components/Items/Item.jsx
@@ -4,16 +4,16 @@ import dummy from '../../../data/data.json'
 import Rating from '@mui/material/Rating';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
-
+import subBanner from '../../assets/subpage/subbanner1.png'
 const Wrapper = styled.div`
     width: 100%;
-    background-color: gold;
     height: 5000px;
 `
 const Banner = styled.div`
     width:100%;
-    height: 300px;
-    background-color: pink;
+    height: 800px;
+    background:url(${subBanner});
+    background-size:100% 100%;
 `
 const ProductInformation = styled.div`
     width:90%;
@@ -51,25 +51,26 @@ const FilterNew = styled.button`
 `
 const ReviewsBox = styled.div`
     width: 80%;
-    margin:20px auto;
-    background-color: pink;
-ul{
-    width:100%;
-}
- li {
+    margin:20px auto;    
+    ul{
+        width:100%;
+    }
+
+.reviewList {
   width:100%;
   margin: 30px 0;
   border:1px solid blue;
+  background-color: #D9D9D9;
   border-radius: 20px;
   display: flex;
   flex-wrap: wrap;
   padding: 20px;
 }
-.profil{
+.profile{
     border: 1px solid blue;
     width:100px;
     height: 100px;
-    margin-bottom: 10px;
+    margin-bottom: 30px;
     img {
         width:100%;
     }
@@ -86,15 +87,7 @@ ul{
     border-radius: 20px;
     padding: 20px;
 }
-.imgBox {
-    flex-wrap:wrap;
-    border: 1px solid blue;
-    width:100%;
-    height: 100px;
-    img{
-        width:100px;
-    }
-}
+
 `
 
 const MyReivewForm = styled.form`
@@ -105,7 +98,7 @@ const MyReivewForm = styled.form`
 `
 
 function Item(value){
-    // const [value , setValue] = useState("")
+
     return(
         <Wrapper>
             <Banner/>
@@ -120,39 +113,27 @@ function Item(value){
             </FilterButtonBox>
             <ReviewsBox>
                 <ul>
-                {dummy.reviews.map((review , i) => {
-                    let photos = review.photo.map((item) => {
-                        return item.img;
-                    })
+                {dummy.reviews.map((review) => {
                     return (
                             <li className="reviewList" key={review.id}> 
-                                <div className="profil">
+                                <div className="profile">
                                     <img src={review.avatar} />
                                     <p className="userName">{review.name}</p>
                                 </div>
-                                <Rating
-                                    name="simple-controlled"
-                                    value={value}
-                                    onChange={(event, newValue) => {
-                                        setValue(newValue);
-                                    }}
-                                />
-                                <div className="imgBox">
-                                    <img src={review.url} />
-                                </div>
-                                <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
-                                    {photos.map((item) => (
+                                <Rating name="read-only" value={value} size="large" readOnly />
+                                <ImageList sx={{ width: 230, height: 230 }} cols={5} rowHeight={"auto"} gap={10}>
+                                    {review.photo && review.photo.map((item) => (
                                         <ImageListItem key={item.img}>
-                                        <img
+                                        <img 
                                             src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
                                             srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
                                             alt={item.title}
                                             loading="lazy"
-                                        />
+                                            />
                                         </ImageListItem>
                                     ))}
                                     </ImageList>
-                                <div className="commentBox">{review.comment}</div> 
+                                <div className="commentBox">{review.comment}</div>
                             </li>
                     )
                 })}
@@ -160,6 +141,13 @@ function Item(value){
             </ReviewsBox>
             <MyReivewForm>
                 <div><img></img></div>
+                <Rating
+                    name="simple-controlled"
+                    value={value}
+                    onChange={(event, newValue) => {
+                        setValue(newValue);
+                    }}
+                />
                 <input type="text" />
                 <button name="bookmark">Heart</button>
                 <button name="submit" type="submit">작성</button>
