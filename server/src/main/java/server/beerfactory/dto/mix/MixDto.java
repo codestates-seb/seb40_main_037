880e892aeb01820d809af3fb96e4ff80de5df3cd
homebird9 <107456299+homebird9@users.noreply.github.com>
2022-11-18 17:12:04 +0900

    :art: feat: MixDto 코드 수정

--- server/src/main/java/server/beerfactory/dto/mix/MixDto.java
+++ server/src/main/java/server/beerfactory/dto/mix/MixDto.java
@@ -4,58 +4,53 @@ import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
-import java.util.List;
 
 public class MixDto {
 
     @Getter
+    @Setter
     @AllArgsConstructor
     @NoArgsConstructor
     public static class Post {
 
-        private Long mixId;
         private Long userId;
         @NotBlank(message = "제목을 입력해 주세요.")
         private String title;
         @NotBlank(message = "내용을 입력해 주세요.")
         private String content;
-//        private List<String> image;
+        private String image;
 //        private Boolean mix_vote; 좋아요 미설정
-//        private List<String> tage; 태그 미설정
-    }
-
-    @Getter
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public static class Patch {
-        private long mixId;
-        @NotBlank(message = "제목을 입력해 주세요.")
-        private String title;
-        @NotBlank(message = "내용을 입력해 주세요.")
-        private String content;
-//        private List<String> image;
-//        private Boolean mix_vote; 좋아요 미설정
-//        private List<String> tage; 태그 미설정
-
-        public void setMixId(long mixId) {
-            this.mixId = mixId;
-        }
     }
 
     @Getter
     @Setter
     @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Patch {
+
+        private long id;
+        @NotBlank(message = "제목을 입력해 주세요.")
+        private String title;
+        @NotBlank(message = "내용을 입력해 주세요.")
+        private String content;
+        private String image;
+//        private Boolean mix_vote; 좋아요 미설정
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
     public static class Response {
-        private long mixId;
+        private long id;
         private String title;
         private String content;
-//        private String image;
-        private int likeCount;
-        private int dislikeCount;
+        private String image;
+        private Integer likeCount;
+        private Integer dislikeCount;
         private LocalDateTime createdAt;
         private LocalDateTime modifiedAt;
 //        private Boolean mix_vote; 좋아요 미설정
-//        private List<String> tage; 태그 미설정
     }
 
 }
