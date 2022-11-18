3c3b1f137f432e9b4ebf2e2025e027f46a59640d
homebird9 <107456299+homebird9@users.noreply.github.com>
2022-11-18 17:11:25 +0900

    :art: feat: MixRepository 코드 수정

--- server/src/main/java/server/beerfactory/repository/mix/MixRepository.java
+++ server/src/main/java/server/beerfactory/repository/mix/MixRepository.java
@@ -3,6 +3,10 @@ package server.beerfactory.repository.mix;
 import org.springframework.data.jpa.repository.JpaRepository;
 import server.beerfactory.entity.mix.Mix;
 
+import java.util.Optional;
+
 
 public interface MixRepository extends JpaRepository<Mix, Long> {
+    @Override
+    Optional<Mix> findById(Long mixId);
 }
