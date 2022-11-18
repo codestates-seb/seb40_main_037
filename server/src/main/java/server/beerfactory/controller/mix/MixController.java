3157aaede6c89bdabf470a72162373933063caec
homebird9 <107456299+homebird9@users.noreply.github.com>
2022-11-18 17:12:24 +0900

    :art: feat: MixController 코드 수정

--- server/src/main/java/server/beerfactory/controller/mix/MixController.java
+++ server/src/main/java/server/beerfactory/controller/mix/MixController.java
@@ -1,6 +1,5 @@
 package server.beerfactory.controller.mix;
 
-import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,75 +16,69 @@ import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 import java.util.List;
 
-@SuppressWarnings("ALL")
 @RestController
-@RequestMapping("/mix")
+@RequestMapping("/mixes")
 @Validated
-@AllArgsConstructor
 public class MixController {
     private final MixService mixService;
-    private final MixMapper mapper;
-//    private final UserService userService;
+    private final MixMapper mixMapper;
 
+    public MixController(MixService mixService, MixMapper mixMapper) {
+        this.mixService = mixService;
+        this.mixMapper = mixMapper;
+    }
 
-/*    @PostMapping
-    public ResponseEntity postMix(@Valid @RequestBody MixDto.Post requestBody) {
-        User findUser = userService.findUser(requestBody.getUserId());
-        Mix mix = mapper.mixPostDtoToMix(requestBody);
-        mix.setUser(findUser);
-        Mix createdMix = mixService.createMix(mix);
-
-        return new ResponseEntity<>(new SingleResponseDto<>(mix), HttpStatus.CREATED);
-    }*/
 
     @PostMapping
     public ResponseEntity postMix(@Valid @RequestBody MixDto.Post requestBody) {
-        Mix mix = mapper.mixPostDtoToMix(requestBody);
-        Mix createdMix = mixService.createMix(mapper.mixPostDtoToMix(requestBody));
-
-        return new ResponseEntity<>(new SingleResponseDto<>(mapper.mixPostDtoToMix(requestBody)),
+        Mix mix = mixMapper.mixPostDtoToMix(requestBody);
+        Mix createdMix = mixService.createMix(mix);
+        MixDto.Response response = mixMapper.mixToMixResponse(createdMix);
+        return new ResponseEntity<>(new SingleResponseDto<>(response),
                 HttpStatus.CREATED);
     }
 
 
     @PatchMapping("/{mix-id}")
-    public ResponseEntity patchMix(
-            @PathVariable("mix-id") @Positive long mixId,
-            @Valid @RequestBody MixDto.Patch requestBody) {
-        requestBody.setMixId(mixId);
-        Mix mix = mixService.updateMix(mapper.mixPatchDtoToMix(requestBody));
+    public ResponseEntity patchMix(@PathVariable("mix-id") @Positive long id,
+                                   @Valid @RequestBody MixDto.Patch requestBody) {
+        requestBody.setId(id);
+        Mix mix = mixMapper.mixPatchDtoToMix(requestBody);
         Mix updatedMix = mixService.updateMix(mix);
+        MixDto.Response response = mixMapper.mixToMixResponse(updatedMix);
 
-        return new ResponseEntity<>(new SingleResponseDto<>(mapper.mixToMixResponse(mix)),
-                HttpStatus.OK);
+        return new ResponseEntity<>(
+                new SingleResponseDto<>(response),HttpStatus.OK
+        );
+
     }
 
     @GetMapping("/{mix-id}")
-    public ResponseEntity getMix(@PathVariable("mix-id") @Positive long mixId) {
+    public ResponseEntity getMix(@PathVariable("mix-id") Long mixId) {
         Mix mix = mixService.findMix(mixId);
-
-        return new ResponseEntity<>(
-                new SingleResponseDto<>(mapper.mixToMixResponse(mix)),
-                HttpStatus.OK);
+        
+        return new ResponseEntity(
+                new SingleResponseDto<>(mixMapper.mixToMixResponse(mix)),
+                HttpStatus.OK
+        );
     }
-
+    
     @GetMapping
     public ResponseEntity getMixes(@Positive @RequestBody int page,
                                    @Positive @RequestBody int size) {
         Page<Mix> pageMixes = mixService.findMixes(page - 1, size);
         List<Mix> mixes = pageMixes.getContent();
-
+        
         return new ResponseEntity<>(
-                new MultiResponseDto<>(mapper.mixesToMixResponse(mixes), pageMixes),
-                HttpStatus.OK
-        );
+                new MultiResponseDto<>(mixMapper.mixesToMixResponseDto(mixes),
+                        pageMixes),
+                HttpStatus.OK);
     }
-
+    
     @DeleteMapping("/{mix-id}")
-    public ResponseEntity deleteMix(@PathVariable("mix-id") @Positive long mixId) {
-        mixService.deleteMix(mixId);
+    public ResponseEntity deleteMix(@PathVariable("mix-id") Long id) {
+        mixService.deleteMix(id);
+        
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
-
-
