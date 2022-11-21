2efcf52b1b49b4b385512945abbbe726a9a23a1b
homebird9 <107456299+homebird9@users.noreply.github.com>
2022-11-21 11:14:40 +0900

    :sparkles: feat: MixReply Page 코드 추가

--- server/src/main/java/server/beerfactory/repository/mix/MixReplyRepository.java
+++ server/src/main/java/server/beerfactory/repository/mix/MixReplyRepository.java
@@ -1,5 +1,7 @@
 package server.beerfactory.repository.mix;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import server.beerfactory.entity.mix.MixReply;
 
@@ -8,4 +10,7 @@ import java.util.Optional;
 public interface MixReplyRepository extends JpaRepository<MixReply, Long> {
     @Override
     Optional<MixReply> findById(Long aLong);
+
+    //외래키로 찾을때는 find + By + {기본키 엔티티(첫글자는 대문자로)} + {외래키 필드명(첫글자는 대문자로)}
+    public Page<MixReply> findByMix_MixId(long mixId, Pageable pageable);
 }
