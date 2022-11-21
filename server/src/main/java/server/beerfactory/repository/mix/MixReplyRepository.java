0536d60b9d840a89afd9dca08d10dda3a921b54b
homebird9 <107456299+homebird9@users.noreply.github.com>
2022-11-21 14:34:21 +0900

    :zap: feat: 불필요 코드 삭제

--- server/src/main/java/server/beerfactory/repository/mix/MixReplyRepository.java
+++ server/src/main/java/server/beerfactory/repository/mix/MixReplyRepository.java
@@ -9,8 +9,6 @@ import java.util.Optional;
 
 public interface MixReplyRepository extends JpaRepository<MixReply, Long> {
     @Override
-    Optional<MixReply> findById(Long aLong);
+    Optional<MixReply> findById(Long mixReplyId);
 
-    //외래키로 찾을때는 find + By + {기본키 엔티티(첫글자는 대문자로)} + {외래키 필드명(첫글자는 대문자로)}
-    public Page<MixReply> findByMix_MixId(long mixId, Pageable pageable);
 }
