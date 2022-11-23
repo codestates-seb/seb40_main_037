6f7db453a40c71b6efe96ad2c5d89bbcad4e7fa2
Heeyoon <107894669+Heeyoon0214@users.noreply.github.com>
2022-11-23 10:18:20 +0900

    :sparkles: feat: 로그인/ 회원가입 레이아웃 구현, Beer request 레이아웃 구현

--- client/src/components/Layout/style.js
+++ client/src/components/Layout/style.js
@@ -1,5 +1,6 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { container } from '../../styles';
+
 export const Container = styled.main`
   position: relative;
   ${container};
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+import Signin from '../components/Signin';
+
+const Login = () => {
+  return (
+    <Layout>
+      <Signin />
+    </Layout>
+  );
+};
+
+export default Login;
