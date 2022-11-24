36b0db01e92e28f6e737cf209ed4126993732c44
homebird9 <107456299+homebird9@users.noreply.github.com>
2022-11-24 16:03:48 +0900

    :sparkles: feat: MixVoteController 생성

---
+++ server/src/main/java/server/beerfactory/controller/mix/MixVoteController.java
@@ -0,0 +1,28 @@
+/*package server.beerfactory.controller.mix;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import server.beerfactory.entity.user.User;
+import server.beerfactory.service.mix.MixVoteService;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/mixes")
+@Validated
+public class MixVoteController {
+    private final MixVoteService mixVoteService;
+
+    @PostMapping("/like")
+    public int postMixVote(@PathVariable("mix-id") Long id,
+                           @Min(1) @Max(3) @PathVariable("num") int num) {
+        return mixVoteService.mixVote(id, num, user);
+    }
+
+}*/
