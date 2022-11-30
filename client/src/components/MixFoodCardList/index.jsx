c3f0adcb332c40f24188def8109fad90ef869891
roxpray <si7531@naver.com>
2022-11-30 15:49:29 +0900

    Mix API 작업

--- client/src/components/MixFoodCardList/index.jsx
+++ client/src/components/MixFoodCardList/index.jsx
@@ -11,49 +11,109 @@ import {
   CardTime,
 } from './style';
 import { Link } from 'react-router-dom';
-import Mixdummydata from '../../../data/Mixdummydata.json';
 import { relTimeFormat } from '../../util/convertor';
+import { useState, useEffect } from 'react';
+import Loading from '../Loading/Loading';
+import { fetchMixList } from '../../util/fetchMix';
+import MixPagination from './Pagination';
 
-const MixFoodCardList = () => {
+function MixFoodCardList() {
+  const [MixList, setMixList] = useState([]);
+  const [pageInfo, setPageInfo] = useState({});
+  const [isPending, setIsPending] = useState(true);
+  const [currPage, setCurrPage] = useState(0);
+  const [isUpdate, setIsUpdate] = useState(true);
+
+  function onPageChange(e) {
+    setCurrPage(e.target);
+    setIsUpdate(true);
+  }
+  useEffect(() => {
+    if (isUpdate) {
+      fetchMixList(currPage).then(res => {
+        setMixList(res.data);
+        setPageInfo(res);
+        setIsPending(false);
+        setIsUpdate(false);
+      });
+    }
+  }, [isUpdate]);
+
+  if (isPending) {
+    return (
+      <div>
+        <Loading />
+      </div>
+    );
+  }
+
+  const onClickPage = target => {
+    if (target === 'Prev') {
+      setCurrPage(currPage - 1);
+    } else if (target === 'Next') {
+      setCurrPage(currPage + 1);
+    } else {
+      setCurrPage(+target);
+    }
+    setIsUpdate(true);
+  };
+  console.log(MixList);
+  console.log(pageInfo);
+
   return (
     <Wrapper>
-      <ul>
-        {Mixdummydata.card &&
-          Mixdummydata.card.map(card => {
-            return (
-              <li className="cardList" key={card.id}>
-                <CardBox>
-                  <CardImageBox>
-                    <CardImage src={card.cardImg} alt="임시 사진입니다." />
-                  </CardImageBox>
-                  <CardBottom>
-                    <CardIDImage src={card.avatar} alt="임시 사진입니다." />
-                    <Link to="/mypage">
-                      <CardIDTitle>{card.nickname}</CardIDTitle>
-                    </Link>
-                    <CardVote>추천 {card.votes}</CardVote>
-                  </CardBottom>
-                  <CardBottom>
-                    <Link to="/">
-                      <CardTitle>
-                        {card.title && card.title.replace(/"/g, '').replace(/<[^>]*>?/g, '')}
-                      </CardTitle>
-                    </Link>
-                    <CardTime>
-                      {card.modified !== null ? (
-                        <h1>{relTimeFormat(card.modified)} 수정됨</h1>
-                      ) : (
-                        <h1>{relTimeFormat(card.createdAt)} 생성됨 </h1>
-                      )}
-                    </CardTime>
-                  </CardBottom>
-                </CardBox>
-              </li>
-            );
-          })}
-      </ul>
+      <div>
+        <ul>
+          {MixList &&
+            MixList.map(MixList => {
+              return (
+                <li className="cardList" key={MixList.id}>
+                  <CardBox>
+                    <CardImageBox>
+                      <CardImage src={MixList.image} alt="임시 사진입니다." />
+                    </CardImageBox>
+                    <CardBottom>
+                      {/* <CardIDImage src={MixList.avatar} alt="임시 사진입니다." /> */}
+                      <Link to="/mypage">
+                        {/* <CardIDTitle>{MixList.nickname}</CardIDTitle> */}
+                      </Link>
+                      <CardVote>좋아요 {MixList.likeCount}</CardVote>
+                    </CardBottom>
+                    <CardBottom>
+                      <Link to="/">
+                        <CardTitle>
+                          {MixList.title &&
+                            MixList.title.replace(/"/g, '').replace(/<[^>]*>?/g, '')}
+                        </CardTitle>
+                      </Link>
+                      <CardTime>
+                        {MixList.modifiedAt !== null ? (
+                          <h1>{relTimeFormat(MixList.modifiedAt)} 수정됨</h1>
+                        ) : (
+                          <h1>{relTimeFormat(MixList.createdAt)} 생성됨 </h1>
+                        )}
+                      </CardTime>
+                    </CardBottom>
+                  </CardBox>
+                </li>
+              );
+            })}
+        </ul>
+        <div>
+          {pageInfo && (
+            <MixPagination
+              currentPage={currPage}
+              pageInfo={pageInfo}
+              setCurrPage={setCurrPage}
+              setIsUpdate={setIsUpdate}
+              onPageChange={onPageChange}
+              onClickPage={onClickPage}
+            />
+          )}
+        </div>
+      </div>
     </Wrapper>
   );
-};
+}
 
 export default MixFoodCardList;
