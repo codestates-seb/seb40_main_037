654c528d625bd10f947ad3646bfda31d71916d4e
Jeen Sung <heee1219@naver.com>
2022-12-01 13:13:24 +0900

    feat: beer 구현

--- server/src/main/java/server/beerfactory/security/auth/BeerfactoryUserDetailsService.java
+++ server/src/main/java/server/beerfactory/security/auth/BeerfactoryUserDetailsService.java
@@ -1,25 +1,28 @@
 //package server.beerfactory.security.auth;
 //
+//import lombok.Builder;
+//import lombok.Getter;
 //import lombok.RequiredArgsConstructor;
 //import org.springframework.security.core.GrantedAuthority;
 //import org.springframework.security.core.userdetails.UserDetails;
 //import org.springframework.security.core.userdetails.UserDetailsService;
 //import org.springframework.security.core.userdetails.UsernameNotFoundException;
 //import org.springframework.stereotype.Component;
+//import org.springframework.stereotype.Service;
 //import server.beerfactory.entity.user.User;
 //import server.beerfactory.exception.BusinessLogicException;
 //import server.beerfactory.exception.ExceptionCode;
-//import server.beerfactory.repository.User.UserRepository;
-//import server.beerfactory.security.utils.BeerfactoryAuthorityUtils;
+//import server.beerfactory.repository.user.UserRepository;
 //
+//import java.util.ArrayList;
 //import java.util.Collection;
 //import java.util.Optional;
 //
 //@RequiredArgsConstructor
-//@Component
+//@Service
 //public class BeerfactoryUserDetailsService implements UserDetailsService {
+//
 //    private final UserRepository userRepository;
-//    private final BeerfactoryAuthorityUtils authorityUtils;
 //
 //    @Override
 //    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -29,18 +32,29 @@
 //        return new BeerfactoryUserDetails(findUser);
 //    }
 //
-//    private final class BeerfactoryUserDetails extends User implements UserDetails {
-//        BeerfactoryUserDetails(User user) {
-//            setId(user.getId());
-//            setEmail(user.getEmail());
-//            setPassword(user.getPassword());
-//            setNickname(user.getNickname());
-//            setRoles(user.getRoles());
+//    @RequiredArgsConstructor
+//    @Getter
+//    @Builder
+//    private static final class BeerfactoryUserDetails extends User implements UserDetails {
+//
+//        private static final long serialVersionUID = 1L;
+//
+//        private User user;
+//
+//        public BeerfactoryUserDetails(User user){
+//            this.user = user;
 //        }
 //
 //        @Override
 //        public Collection<? extends GrantedAuthority> getAuthorities() {
-//            return authorityUtils.createAuthorities(this.getRoles());
+//            Collection<GrantedAuthority> collector = new ArrayList<>();
+//            collector.add(() -> user.getRole());
+//            return collector;
+//        }
+//
+//        @Override
+//        public String getPassword(){
+//            return user.getPassword();
 //        }
 //
 //        @Override
