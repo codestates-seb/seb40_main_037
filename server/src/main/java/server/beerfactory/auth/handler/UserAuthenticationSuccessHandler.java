fa4c92fd8336c74c3a7f3c4bbf30e7990266608e
sean8430 <sean8430@gmail.com>
2022-12-06 17:55:58 +0900

    feat: 로그인 성공시 responseBody에 userId 전달

--- server/src/main/java/server/beerfactory/auth/handler/UserAuthenticationSuccessHandler.java
+++ server/src/main/java/server/beerfactory/auth/handler/UserAuthenticationSuccessHandler.java
@@ -1,8 +1,11 @@
 package server.beerfactory.auth.handler;
 
+import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import server.beerfactory.entity.user.User;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,5 +19,10 @@ public class UserAuthenticationSuccessHandler implements AuthenticationSuccessHa
                                         Authentication authentication) throws IOException {
         // 인증 성공 후, 로그를 기록하거나 사용자 정보를 response로 전송하는 등의 추가 작업을 할 수 있다.
         log.info("# Authenticated successfully!");
+
+        User user = (User) authentication.getPrincipal();
+        Gson gson = new Gson();
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(gson.toJson(user.getId()));
     }
 }
